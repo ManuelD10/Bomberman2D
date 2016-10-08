c29745568dcc7988bdccce0cdd022a60066875fb
Carlos Herrera <ceherrera@rtvc.gov.co>
2016-10-08 08:30:10 -0500

    enemigo y muralla 2

--- Assets/Scripts/GeneradorParedesDes.cs
+++ Assets/Scripts/GeneradorParedesDes.cs
@@ -6,16 +6,19 @@ public class GeneradorParedesDes : MonoBehaviour
 		public GameObject paredPrefab;
 
 		// Use this for initialization
-		void Start () {
-		int posX;
-		int posY;
-		int numParedes = 20;
-		for ( int i=0; i<numParedes;i++)
+		void Start () 
 		{
-			posX = 2 * Random.Range (-3, 3);
-			posY = 2 * Random.Range (-3, 3);
-			Instantiate(paredPrefab, new Vector3(posX,posY), transform.rotation);
-					}
+			int posX;
+			int posY;
+			int numParedes = 30;
+			for ( int i=0; i<numParedes;i++)
+			{
+				//	posX = 2 * Random.Range (-3, 3);
+				//	posY = 2 * Random.Range (-3, 3);
+				posX = Random.Range (-6, 6);
+				posY = Random.Range (-6, 6);
+				Instantiate(paredPrefab, new Vector3(posX,posY), transform.rotation);
+			}
 		}
 
 		// Update is called once per frame
