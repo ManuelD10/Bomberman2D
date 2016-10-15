9150acadd1be23e4d220e9724fc65aa5e8049b39
ManuelD10 <manuel-d10@hotmail.com>
2016-10-15 10:42:37 -0500

    Movimiento derecha izquierda

--- Assets/Scripts/MovBomber.cs
+++ Assets/Scripts/MovBomber.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class MovBomber : MonoBehaviour {
 
+<<<<<<< Updated upstream
 	// Use this for initialization
 	void Start () {
 
@@ -11,6 +12,70 @@ public class MovBomber : MonoBehaviour {
 
 
 	void Update () {
+=======
+	private Animator miAnimator;
+	float velocidadMovimiento;
+
+	// Use this for initialization
+	void Start () 
+	{
+		miAnimator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		// Movimiento a la derecha
+		if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			if (miAnimator.GetBool("caminarDerecha") == true)
+			{
+				miAnimator.SetBool ("caminarDerecha", false);
+			}
+			else
+			{
+				miAnimator.SetBool ("caminarDerecha", true);
+			}
+		}
+
+		if (Input.GetKeyUp (KeyCode.RightArrow)) 
+		{
+			if (miAnimator.GetBool("caminarDerecha") == true)
+			{
+				miAnimator.SetBool ("caminarDerecha", false);
+			}
+			else
+			{
+				miAnimator.SetBool ("caminarDerecha", true);
+			}
+		}
+
+		// Movimiento a la izquierda
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) 
+		{
+			if (miAnimator.GetBool("caminarIzquierda") == true)
+			{
+				miAnimator.SetBool ("caminarIzquierda", false);
+			}
+			else
+			{
+				miAnimator.SetBool ("caminarIzquierda", true);
+			}
+		}
+
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) 
+		{
+			if (miAnimator.GetBool("caminarIzquierda") == true)
+			{
+				miAnimator.SetBool ("caminarIzquierda", false);
+			}
+			else
+			{
+				miAnimator.SetBool ("caminarIzquierda", true);
+			}
+		}
+>>>>>>> Stashed changes
 
 	}
 
@@ -19,4 +84,8 @@ public class MovBomber : MonoBehaviour {
 		var movimiento = new Vector3(Mathf.Round(Input.GetAxis("Horizontal")), Mathf.Round(Input.GetAxis("Vertical")), 0);
 		transform.position += movimiento * velocidadMovimiento * Time.deltaTime;
 	}
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
